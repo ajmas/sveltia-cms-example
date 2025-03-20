5e9f3c34c1de1b8c486fe1062b2e4d076ce0a9c7
Andre-John Mas <andrejohn.mas@gmail.com>
2025-03-20 15:30:14 -0400

    adjusted config and provide two recipes

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ npm run dev
 
 Connect to http://127.0.0.1:8000
 
+## Git-Gateway
+
+The example leverages the [netlify/git-gateway](https://github.com/netlify/git-gateway) project
+for the backend. Instructions on getting a basic installing up and running, assuming
+are in the git-gateway project folder:
+
+  - Install 'go'
+    - macOS with Homebrew: `brew install go`
+  - Install dependencies `go install`
+  - Set up your environment variables, using `example.env` as reference
+  - Launch the server: `go run main.go -c example.env`
--- src/main.js
+++ src/main.js
@@ -11,14 +11,14 @@ init({
       name: 'git-gateway',
     },
     load_config_file: false,
-    media_folder: "static/images/uploads",
-    public_folder: "/images/uploads",
+    media_folder: 'static/images/uploads',
+    public_folder: '/images/uploads',
     collections: [
-      { label: "Blog", name: "blog", folder: "_posts/blog", create: true, fields: [
-        { label: "Title", name: "title", widget: "string" },
-        { label: "Publish Date", name: "date", widget: "datetime" },
-        { label: "Featured Image", name: "thumbnail", widget: "image" },
-        { label: "Body", name: "body", widget: "markdown" },
+      { label: 'Blog', name: 'blog', folder: '_posts/blog', create: true, fields: [
+        { label: 'Title', name: 'title', widget: 'string' },
+        { label: 'Publish Date', name: 'date', widget: 'datetime' },
+        { label: 'Featured Image', name: 'thumbnail', widget: 'image' },
+        { label: 'Body', name: 'body', widget: 'markdown' },
       ]},
     ],
   },
